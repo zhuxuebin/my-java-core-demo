9d27d92f8f18a73a3ec996ea5097ec64204fb2e0
zhuxuebin <zhuxuebin2014@163.com>
2018-11-25 23:59:33 +0800

    jvm invoke method

---
+++ src/main/java/com/zxb/jvm/core/j05methodinvoke/InvokeSample01.java
@@ -0,0 +1,40 @@
+package com.zxb.jvm.core.j05methodinvoke;
+
+import java.util.ArrayList;
+
+/**
+ * @Description TODO
+ * Created by xuery on 2018/11/25.
+ */
+public class InvokeSample01 {
+
+    public static void main(String[] args) {
+        InvokeSample01 invokeSample01 = new InvokeSample01();
+        invokeSample01.invoke(null,1);
+        invokeSample01.invoke(null,1,2);
+        invokeSample01.invoke(null, new Object[]{1});
+
+        invokeSample01.method1(1, null);
+        invokeSample01.method1(1, new Object());
+        invokeSample01.method1(new Integer(1),null);
+    }
+
+    public void method1(int x, Object obj){
+        System.out.println("int");
+    }
+    public void method1(Integer x, Object obj){
+        System.out.println("Integer");
+    }
+
+    public void method1(int x, Object ... args){
+        System.out.println("args");
+    }
+
+    public void invoke(Object obj, Object ...args){
+        System.out.println("invoke1");
+    }
+
+    public void invoke(String s, Object obj, Object ...args){
+        System.out.println("invoke2");
+    }
+}
